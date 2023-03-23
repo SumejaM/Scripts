f406ae339969c5eb047f061af16f8ed9118a0d44
SumejaM <sumeja.mahic.11@gmail.com>
2023-03-23 14:34:00 +0100

    otvaranje web stranice

---
+++ bash/zadatak4.bat
@@ -0,0 +1,8 @@
+@echo off
+start msedge "https://it-lab.ba/"
+pause
+start chrome "https://it-lab.ba/"
+pause
+start "C:\Program Files (x86)\Microsoft\Edge\Application\msedge" "https://it-lab.ba/"
+echo "IT-lab otvoren u chromu i edgu"
+pause
